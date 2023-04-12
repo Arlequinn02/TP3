c0419fac410f1f69f0b51a56fc78c1b828fa04c2
Arlequinn02 <william.odelot02gmail.com>
2023-04-12 12:12:47 +0200

    Ca marche normalement

--- test/PhpTest.php
+++ test/PhpTest.php
@@ -1,10 +1,40 @@
 <?php
-/**YES**/
+/**
+ * Recipe class file
+ *
+ * PHP Version 5.2
+ *
+ * @category Recipe
+ * @package  Recipe
+ * @author   Lorna Jane Mitchell <lorna@ibuildings.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://example.com/recipes
+ */
+
+/**
+ * Validateur class
+ *
+ * The class holding the root Recipe class definition
+ *
+ * @category Validateur
+ * @package  Validateur
+ * @author   Lorna Jane Mitchell <lorna@ibuildings.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://example.com/recipes/recipe
+ */
+
 namespace app;
-/**Ceci est une classe**/
+
 class Validateur
 {
-    /**Ceci est une fonction**/
+    /**
+     * Get the ingredients
+     *
+     * This function calls a static fetching method against the Ingredient class
+     * and returns everything matching this recipe ID
+     *
+     * @return array An array of Ingredient objects
+     */
     public function check($input)
     {
         if (strlen($input) < 2) {
