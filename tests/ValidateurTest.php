df2a917829b89f75521e4bfa24946bf0d2093254
Arlequinn02 <william.odelot02gmail.com>
2023-04-26 17:28:56 +0200

    Nouvelle Action

--- tests/ValidateurTest.php
+++ tests/ValidateurTest.php
@@ -1,14 +1,14 @@
 <?php
-use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
-require_once 'src/Validateur.php';
+require_once '..\src\Validateur.php';
 
 final class ValidateurTest extends TestCase
 {
     public function testcheck()
     {
-        $this->assertTrue(false);
-        $this->assertFalse(true);
+        $v = new \app\Validateur();
+        $this->assertTrue($v->check('roadbike'));
+        $this->assertFalse($v->check('a'));
     }
 }
